2fe4a7e6b678b33112669f2be9a1b3e68d0675ca
katoyudai <202410ap01_y.kato@x-ido.sakura.ne.jp>
2025-02-06 14:01:36 +0900

    #34749
    対応するデータの書類を押しファイルマネージャー画面に遷移した際登録データの物件名と部屋が表示されていない状態
    原因49行目を削除し物件名と部屋を表示したいので132行目form.articleNo.value='<?php print $articleNo ?>の部分をform.sName.value='<?php print $article ?>に替え<?php print $article ?>を削除しform.sRoom.value='<?php print $room ?>に変更で不具合修正

--- SRC/libArticle.php
+++ SRC/libArticle.php
@@ -46,7 +46,8 @@ function subArticle()
 		<input type="hidden" name="sPage" value="<?php print $sPage ?>" />
 		<input type="hidden" name="articleNo" />
 		<input type="hidden" name="sName" />
-		<input type="hidden" name="sRoom" />
+
+
 
 		<a href="javascript:form.act.value='articleEdit';form.submit();"><img src="./images/btn_enter.png"></a>
 
@@ -128,7 +129,7 @@ function subArticle()
 					<td class="list_td<?php print $i ?>"><?php print $room ?></td>
 					<td class="list_td<?php print $i ?>"><?php print $drawing ?></td>
 					<td class="list_td<?php print $i ?>"><?php print $keyPlace ?></td>
-					<td class="list_td<?php print $i ?>"><a href="javascript:form.act.value='fManager';form.articleNo.value='<?php print $articleNo ?>';form.submit();"><?php print $article ?></a></td>
+					<td class="list_td<?php print $i ?>"><a href="javascript:form.act.value='fManager';form.sName.value='<?php print $article ?>';form.sRoom.value='<?php print $room ?>';form.submit();">表示</a></td>
 					<td class="list_td<?php print $i ?>"><?php print $keyBox ?></td>
 					<td class="list_td<?php print $i ?>"><?php print $drawing ?></td>
 					<td class="list_td<?php print $i ?>"><?php print $sellCharge ?></td>
